19577fafe689e7a943429473758a7ed8833f08c5
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-05-27 01:09:43 -0300

    agregamos useState

--- src/components/Titulos.jsx
+++ src/components/Titulos.jsx
@@ -1,8 +1,8 @@
-const Titulos = () => {
+const Titulos = (props) => {
     return (
         <section className="text-center my-5">
             <h1 className="display-4">Primera web con react</h1>
-            <h2 className="display-5">Aqui agrego otros componentes</h2>
+            <h2 className="display-5">{props.nuevoTitulo}</h2>
         </section>
     );
 };
