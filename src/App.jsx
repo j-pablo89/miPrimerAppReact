0348eac6420ba72536229ba6c3db8a817c69aade
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-05-27 00:05:06 -0300

    agregamos react-bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Lista from "./components/Lista";
 import Titulos from "./components/Titulos";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   // aqui va la logica
@@ -12,8 +13,11 @@ function App() {
     {/* esto es un comentario del html */}
     {/* <Titulos></Titulos> */}
     <Titulos/>
-    <h3>Servicios</h3>
-    <Lista/>
+    <section className="container">
+      <h3>Servicios</h3>
+      <hr/>
+      <Lista/>
+    </section>
   </>
   );
 }
--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Lista = () => {
     return (
-        <ul>
-            <li>Centro de Salud</li>
-            <li>Paquete de vuelo</li>
-            <li>Servicio de entrenamiento</li>
+        <ul className='list-group'>
+            <li className='list-group-item'>Centro de Salud</li>
+            <li className='list-group-item'>Paquete de vuelo</li>
+            <li className='list-group-item'>Servicio de entrenamiento</li>
         </ul>
     );
 };
