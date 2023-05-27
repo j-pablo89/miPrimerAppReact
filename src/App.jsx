8624eeb8e1225dcfee3b0dbd959cb290db1d15fd
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-05-26 23:40:59 -0300

    agregamos el componente Titulo y Lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import Lista from "./components/Lista";
+import Titulos from "./components/Titulos";
 
 function App() {
-  return <div></div>;
+  // aqui va la logica
+  
+  return (
+  //puedo escribir un poquito de logica
+  //<> es un fragment
+  <>
+    {/* esto es un comentario del html */}
+    {/* <Titulos></Titulos> */}
+    <Titulos/>
+    <h3>Servicios</h3>
+    <Lista/>
+  </>
+  );
 }
 
 export default App;
---
+++ src/components/Lista.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Lista = () => {
+    return (
+        <ul>
+            <li>Centro de Salud</li>
+            <li>Paquete de vuelo</li>
+            <li>Servicio de entrenamiento</li>
+        </ul>
+    );
+};
+
+export default Lista;
